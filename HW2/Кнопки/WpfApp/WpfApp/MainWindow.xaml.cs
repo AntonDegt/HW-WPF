188ccb72a7cb81f110674db0926a8009fb7afe8c
AntonDegt <108671823+AntonDegt@users.noreply.github.com>
2023-01-11 15:03:18 +0300

    Add files via upload

---
+++ HW2/Кнопки/WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement el in mainRoot.Children)
+            {
+                if (el is Button)
+                {
+                    Button btn = (Button)el;
+                    btn.Click += Button_Click;
+                    btn.Background = Brushes.SkyBlue;
+                }
+            }
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)e.OriginalSource;
+            if ((string)btn.Content == "")
+            {
+                btn.Background = Brushes.SkyBlue;
+                btn.Content = " ";
+            }
+            else
+            {
+                btn.Background = Brushes.Gray;
+                btn.Content = "";
+            }
+        }
+    }
+}
---
+++ HW2/Сейф/WpfApp/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool close = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach(UIElement el in mainRoot.Children)
+            {
+                if (el is Button)
+                    ((Button)el).Click += Button_Click;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            switch (str)
+            {
+                case "C":
+                    textLabel.Text = "";
+                    break;
+
+                case "OK":
+                    if (textLabel.Text == "0000")
+                    {
+                        textLabel.Text = "OPEN!";
+                        close = false;
+                    }
+                    else
+                        textLabel.Text = "";
+                    break;
+
+                default:
+                    if (close)
+                        textLabel.Text += str;
+                    break;
+            }
+        }
+    }
+}
