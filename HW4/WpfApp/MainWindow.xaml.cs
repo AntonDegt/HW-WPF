f7315e756a12dd1c724887498759f166022d2506
AntonDegt <108671823+AntonDegt@users.noreply.github.com>
2023-01-25 19:39:50 +0300

    Add files via upload

---
+++ HW4/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var uri = new Uri("Theme.xaml", UriKind.Relative);
+            ResourceDictionary resourceDict = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resourceDict);
+        }
+    }
+}
